f288aa32c8adedba711c3ecaeed4102f245394fc
Ivan Marquez <ivanaurelio.s258702@cesurformacion.com>
2022-12-19 11:19:58 +0100

    Examen practico diseño de interfaces

--- static/app.js
+++ static/app.js
@@ -1,3 +1,37 @@
-// nombre_del_alumno
+// Iván Aurelio Márquez Morales
 //
 // Completar JS aqui
+
+var modal = document.getElementById("ventanaModal");
+
+var boton = document.getElementById("abrirModal");
+
+var close = document.getElementsByClassName("cerrar")[0];
+
+
+
+boton.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+
+close.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
+
+window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
+
+document.getElementById("datos").addEventListener('submit', validar);
+
+function validar() {
+    alert("Datos mandados correctamente");
+}
